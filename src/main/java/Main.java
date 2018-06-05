a69469582c36b28e240c5889283745841053ae62
Tomasz <kurek.tom@gmail.com>
2018-06-05 11:13:55 +0200

    Add HTMLScraper

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,18 +4,27 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        String searchText = getSearchText();
-        searchText = searchText.isEmpty() ?
-                "Multiwinner Voting: A New Challenge for Social Choice Theory" : searchText;
+    public static void main(String[] args) throws IOException {
+//        String searchText = getSearchText();
+//        searchText = searchText.isEmpty() ?
+//                "Multiwinner Voting: A New Challenge for Social Choice Theory" : searchText;
+//
+//        String filename = searchText.replaceAll("[ :/*?|\"<>.]", "_");
+//
+//        String url = GoogleSearchScraper.getFirstGoogleSearch(searchText);
+//
+//        InputStream in = PdfDownloader.getPdfStream(url);
+//        PdfParser.parseToTxt(in, filename + ".txt");
+//        in = PdfDownloader.getPdfStream(url);
+//        PdfDownloader.downloadPdf(in, filename + ".pdf");
 
+        String searchText = "Distance rationalization of voting rules";
         String filename = searchText.replaceAll("[ :/*?|\"<>.]", "_");
-
-        String url = GoogleSearchScraper.getFirstGoogleSearch(searchText);
+        String link = HTMLScraper.findLinkToPdfOnDblp(searchText);
+        String url = HTMLScraper.findPdfUrl(link);
 
         InputStream in = PdfDownloader.getPdfStream(url);
         PdfDownloader.downloadPdf(in, filename + ".pdf");
-
         in = PdfDownloader.getPdfStream(url);
         PdfParser.parseToTxt(in, filename + ".txt");
     }
